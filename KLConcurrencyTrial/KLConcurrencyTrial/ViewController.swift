ece209a2e05b8b7d70dd10c06a78fa74e2cb7b55
Oparin Oleg <oa_oparin@mail.ru>
2022-07-12 12:50:38 +0300

    Add files via upload

---
+++ KLConcurrencyTrial/KLConcurrencyTrial/ViewController.swift
@@ -0,0 +1,31 @@
+import UIKit
+import OSLog
+
+class ViewController: UIViewController {
+
+    private var productsProvider: ProductsProvider?
+    private let logger = Logger()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let productsRequester = ProductsRequesterImpl()
+        productsProvider = ProductsProviderImpl(requester: productsRequester)
+    }
+
+    @MainActor
+    @IBAction func loadButtonTapped(_ sender: Any) {
+        Task {
+            let products = (try? await productsProvider?.getProducts()) ?? []
+            
+            guard !products.isEmpty else {
+                logger.debug("Received empty products list 😒")
+                return
+            }
+            
+            logger.debug("Products loaded successfully 😃")
+        }
+    }
+    
+}
+
